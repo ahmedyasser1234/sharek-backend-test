bba747fd856b7e4a204ee38317879871cd79c813
ahmedyasser1234 <ahmedyasser697@gmail.com>
2025-12-16 16:35:09 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
       'http://sharik-sa.com',
       'http://localhost:3000',
       'http://localhost:3001',
-      'http://localhost:5173' 
+      'http://localhost:5173'   
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
@@ -59,9 +59,9 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
   logger.log(' Swagger جاهز على /docs');
 
-  const adminService = app.get(AdminService);
-  await adminService.ensureDefaultAdmin();
-  logger.log(' تم التأكد من وجود الأدمن الأساسي');
+ // const adminService = app.get(AdminService);
+ // await adminService.ensureDefaultAdmin();
+ // logger.log(' تم التأكد من وجود الأدمن الأساسي');
 
   const port = process.env.PORT ?? 3000;
   
